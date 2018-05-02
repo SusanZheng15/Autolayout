2188446b2a1676c2a321ee92d6db7ddcc840058c
SusanZheng15 <zheng.susan12@gmail.com>
2018-05-02 10:13:54 -0400

    first init

---
+++ AutoLayout1/GPColor.swift
@@ -0,0 +1,13 @@
+//
+//  GPColor.swift
+//  AutoLayout1
+//
+//  Created by Susan Zheng on 5/1/18.
+//  Copyright © 2018 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class GPColor: NSObject {
+
+}
